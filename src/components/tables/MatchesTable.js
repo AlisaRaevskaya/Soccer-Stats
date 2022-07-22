c75b34a8245194dd1662749c5291859803ba0f82
Alisa Raevskaya <alisa@madebyforward.com>
2022-07-22 13:50:50 +0300

    Tweak matches table

--- src/components/tables/MatchesTable.js
+++ src/components/tables/MatchesTable.js
@@ -2,44 +2,97 @@
 
 import React from "react";
 
-export default function MatchesTable(props) {
+const MatchesTable = (props) => {
+  const setTime = (dt) => {
+    let d = new Date(dt);
+    let minutes =
+      d.getUTCMinutes() == "0" ? d.getUTCMinutes() + "0" : d.getUTCMinutes();
+    return d.getUTCHours() + " : " + minutes;
+  };
+
+  const setDate = (dt) => {
+    let d = new Date(dt);
+    let month = d.getMonth() + 1;
+    let date = `${d.getDate()}-${month}-${d.getFullYear()}`;
+    return date;
+  };
+
+  const outputScoreIfNotNull = (score1, score2) => {
+    //Отображаются значения только отличные от null
+
+    if (score1 != null || score2 != null) {
+      return (
+        <td>
+          {score1} : {score2}
+        </td>
+      );
+    }
+    return <td> - </td>;
+  };
+
   return (
-    <table >
+    <table>
       <caption>Matches table</caption>
       <thead>
         <tr>
-          <th>Competition</th>
-          <th>Status</th>
-          <th> AwayTeam - homeTeam</th>
-          <th colSpan="3">Score</th>
+          <th className="colored" rowSpan="2">
+            Date
+          </th>
+          <th className="colored" rowSpan="2">
+            Time
+          </th>
+          <th className="colored" rowSpan="2">
+            Status
+          </th>
+          <th className="colored" rowSpan="2">
+            Away Team - Home Team
+          </th>
+          <th colSpan="3" scope="colgroup" className="colored">
+            Scores
+          </th>
         </tr>
         <tr>
-          <th scope="col">FullTime Score </th>
-          <th scope="col">ExtraTime Score </th>
-          <th scope="col">Penalties Score </th>
+          <th scope="col" className="colored">
+            FullTime Score
+          </th>
+          <th scope="col" className="colored">
+            ExtraTime Score
+          </th>
+          <th scope="col" className="colored">
+            Penalties Score
+          </th>
         </tr>
       </thead>
       <tbody>
-        {props.matches.map((item) => (
-          <tr key={item.id}>
-            <td scope="row">{item.competition.name}</td>
-            <td>{item.status}</td>
-            <td>
-              {item.awayTeam.name} - {item.homeTeam.name}
-            </td>
-            <td>
-              {item.score.fullTime.homeTeam} : {item.score.fullTime.awayTeam}
-            </td>
-            <td>
-              {item.score.extraTime.homeTeam} : {item.score.extraTime.awayTeam}
-            </td>
-            <td>
-              {item.score.penalties.homeTeam} : {item.score.penalties.awayTeam}
-            </td>
-          </tr>
-        ))}
+        {props.matches &&
+          props.matches.map((item) => (
+            <tr key={item.id}>
+              <td scope="row">{setDate(item.utcDate)}</td>
+              <td scope="row">{setTime(item.utcDate)}</td>
+              <td>{item.status}</td>
+              <td>
+                {item.awayTeam.name} - {item.homeTeam.name}
+              </td>
+
+              {outputScoreIfNotNull(
+                item.score.fullTime.homeTeam,
+                item.score.fullTime.awayTeam
+              )}
+
+              {outputScoreIfNotNull(
+                item.score.extraTime.homeTeam,
+                item.score.extraTime.awayTeam
+              )}
+
+              {outputScoreIfNotNull(
+                item.score.penalties.homeTeam,
+                item.score.penalties.awayTeam
+              )}
+            </tr>
+          ))}
       </tbody>
-      <tfoot></tfoot>
     </table>
   );
-}
+};
+
+export default MatchesTable;
--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     ],
   },
   devServer: {
-    port: 9000,
+
     hot: true,
     historyApiFallback: true,
   },
